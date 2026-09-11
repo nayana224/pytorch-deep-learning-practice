6d56dac19014889dd11b5555c0cf6841ef0fa148
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:11:56 +0900

    Add U-Net architecture Python scaffold

---
+++ practice/04_unet/unet_architecture.py
@@ -0,0 +1,8 @@
+"""04-1. U-Net architecture practice scaffold."""
+
+# TODO: input tensor / shape 확인
+# TODO: DoubleConv 구현
+# TODO: Down block 구현
+# TODO: Up block 구현
+# TODO: skip connection에서 shape 확인
+# TODO: 작은 U-Net 연결
