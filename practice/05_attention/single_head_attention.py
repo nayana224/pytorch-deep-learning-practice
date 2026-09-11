8d26a37a240ee3599678d02ff84e71ce172e93b5
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:12:10 +0900

    Add single-head attention Python scaffold

---
+++ practice/05_attention/single_head_attention.py
@@ -0,0 +1,8 @@
+"""05-1. Single-head attention practice scaffold."""
+
+# TODO: small token tensor X 준비
+# TODO: Q / K / V projection 구현
+# TODO: QK^T / sqrt(d_k) 계산
+# TODO: softmax attention weight 계산
+# TODO: attention output 계산
+# TODO: attention matrix 시각화
