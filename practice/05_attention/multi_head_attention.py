300c0a9b05fe57905ee90ddd585ed1e8ddb0434b
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:12:16 +0900

    Add multi-head attention Python scaffold

---
+++ practice/05_attention/multi_head_attention.py
@@ -0,0 +1,8 @@
+"""05-2. Multi-head attention practice scaffold."""
+
+# TODO: [B,T,D] 입력 준비
+# TODO: D를 head 수로 분할
+# TODO: [B,H,T,D_head] shape 확인
+# TODO: head별 attention 계산
+# TODO: head concat / output projection
+# TODO: head별 attention map 비교
