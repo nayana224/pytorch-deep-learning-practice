4960793e524f58e5b469a4db31ca5a9100dc9a13
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:12:34 +0900

    Add Tiny ViT Python scaffold

---
+++ practice/07_vit/vit_cifar10.py
@@ -0,0 +1,10 @@
+"""07-2. Tiny ViT / CIFAR-10 practice scaffold."""
+
+# TODO: CIFAR-10 DataLoader
+# TODO: Patch Embedding
+# TODO: CLS token
+# TODO: Positional Embedding
+# TODO: Transformer Encoder
+# TODO: MLP head
+# TODO: train / evaluate
+# TODO: attention / failure case 확인
