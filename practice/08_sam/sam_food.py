c422a8e8545979d8ea9a3fa378e0e971c2f5c4a0
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:12:49 +0900

    Add SAM custom-food Python scaffold

---
+++ practice/08_sam/sam_food.py
@@ -0,0 +1,8 @@
+"""08-2. SAM / Custom Food image practice scaffold."""
+
+# 공식 SAM pretrained model을 사용한다.
+# TODO: custom food image 경로 설정
+# TODO: point / box / automatic mask 비교
+# TODO: tray 포함 여부 확인
+# TODO: sauce / reflection / 경계 누락 failure case 기록
+# TODO: 기존 food mask와 비교
