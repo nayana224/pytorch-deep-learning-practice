10c2f6dd0fc64a7a96968f12f9c9107105385383
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:11:36 +0900

    Add CNN feature-map Python scaffold

---
+++ practice/02_cnn/feature_maps.py
@@ -0,0 +1,7 @@
+"""02-2. CNN filter / feature-map visualization scaffold."""
+
+# TODO: load or train CNN
+# TODO: inspect conv filters
+# TODO: capture feature maps
+# TODO: compare before/after training
+# TODO: save figures to outputs/
