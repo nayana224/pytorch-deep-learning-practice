82578f0ff5a508278592f70901b0c0b75409419e
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:12:23 +0900

    Add Transformer encoder Python scaffold

---
+++ practice/06_transformer/transformer_encoder.py
@@ -0,0 +1,8 @@
+"""06. Transformer encoder practice scaffold."""
+
+# TODO: input token tensor 준비
+# TODO: Multi-Head Attention 연결
+# TODO: residual + LayerNorm 확인
+# TODO: Feed Forward 구현
+# TODO: 두 번째 residual + LayerNorm 확인
+# TODO: encoder block 전체 shape 추적
