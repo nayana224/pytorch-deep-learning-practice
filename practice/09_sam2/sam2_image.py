75de0b8aee949fdcfdb60b4f84b7e687a79eb2a4
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:12:56 +0900

    Add SAM2 image Python scaffold

---
+++ practice/09_sam2/sam2_image.py
@@ -0,0 +1,8 @@
+"""09-1. SAM 2 image inference practice scaffold."""
+
+# 공식 SAM 2 코드를 사용한다.
+# TODO: checkpoint / config 설정
+# TODO: SAM2ImagePredictor 준비
+# TODO: image load
+# TODO: point / box prompt inference
+# TODO: SAM과 사용 흐름 및 결과 비교
