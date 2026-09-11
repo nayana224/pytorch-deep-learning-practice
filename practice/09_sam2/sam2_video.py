f229c19993fd30c301863744dfbb41b7aec9f9ae
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:13:03 +0900

    Add SAM2 video Python scaffold

---
+++ practice/09_sam2/sam2_video.py
@@ -0,0 +1,9 @@
+"""09-2. SAM 2 video segmentation / tracking practice scaffold."""
+
+# 공식 SAM 2 코드를 사용한다.
+# TODO: video frame 경로 설정
+# TODO: video state 초기화
+# TODO: 첫 frame prompt 추가
+# TODO: propagate_in_video 실행
+# TODO: frame별 mask 저장 / 시각화
+# TODO: memory 효과와 failure case 기록
