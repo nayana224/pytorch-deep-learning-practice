c33acfacc1b17cbf5c5dd007479b30fa1b918348
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:11:48 +0900

    Move ResNet CIFAR-10 scaffold to practice

---
+++ practice/03_resnet/resnet18_cifar10.py
@@ -0,0 +1,11 @@
+"""03-2. ResNet / CIFAR-10 practice scaffold."""
+
+# TODO: seed / device
+# TODO: CIFAR-10 DataLoader
+# TODO: input / GT / output / loss 확인
+# TODO: BasicBlock 구현
+# TODO: 작은 Plain CNN 구현
+# TODO: 작은 ResNet 구현
+# TODO: 동일 조건으로 학습
+# TODO: loss / accuracy 비교
+# TODO: prediction / failure case 확인
