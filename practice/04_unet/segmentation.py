45beb1c7cfdb2ed3a0dae89bccc1af822c5c61fc
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:12:01 +0900

    Add U-Net segmentation Python scaffold

---
+++ practice/04_unet/segmentation.py
@@ -0,0 +1,9 @@
+"""04-2. U-Net segmentation practice scaffold."""
+
+# TODO: segmentation dataset 준비
+# TODO: input / GT mask shape 확인
+# TODO: U-Net output shape 확인
+# TODO: loss 설정
+# TODO: 학습 / 평가
+# TODO: IoU / Dice 계산
+# TODO: prediction / overlay / error map 저장
