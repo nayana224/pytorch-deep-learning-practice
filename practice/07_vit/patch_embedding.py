23e6ada3785629d2ae23e4cb619ea43a3d7ce910
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:12:30 +0900

    Add ViT patch embedding Python scaffold

---
+++ practice/07_vit/patch_embedding.py
@@ -0,0 +1,8 @@
+"""07-1. ViT patch embedding practice scaffold."""
+
+# TODO: CIFAR-10-like input [B,3,32,32] 준비
+# TODO: patch size 결정
+# TODO: Conv2d 또는 직접 reshape로 patch embedding 구현
+# TODO: feature grid shape 확인
+# TODO: token sequence [B,N,D]로 변환
+# TODO: patch 시각화
