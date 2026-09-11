e53b871ea4547a59f2b56ad14a156cc871d699ff
inpyo_dev <inpyoi1304@gmail.com>
2026-09-11 16:12:41 +0900

    Add SAM image Python scaffold

---
+++ practice/08_sam/sam_image.py
@@ -0,0 +1,9 @@
+"""08-1. SAM image inference practice scaffold."""
+
+# 공식 SAM pretrained model을 사용한다.
+# TODO: checkpoint / model type 설정
+# TODO: image load
+# TODO: point prompt inference
+# TODO: box prompt inference
+# TODO: mask overlay 저장
+# TODO: prompt 변화에 따른 결과 비교
